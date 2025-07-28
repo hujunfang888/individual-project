50a73bd962cbac9ff10abd3870df5b59f4e7775b
Junfang H <alyjh38@nottingham.ac.uk>
2025-07-28 12:34:50 +0100

    Update and rename MF_hap2_vs_groen_R.sh to MFandBP_hap2_vs_groen_R.sh

--- danica/MFandBP_hap2_vs_groen_R.sh
+++ danica/MFandBP_hap2_vs_groen_R.sh
@@ -84,3 +84,44 @@ venn.diagram(
 )
 
 
+
+
+####BP
+write.table(allRes_groen_BP,
+            file = "topGO_BP_groen_results.tsv",
+            sep = "\t", quote = FALSE, row.names = FALSE)
+
+hap2_bp <- read.delim("topGO_BP_results.tsv", sep = "\t", stringsAsFactors = FALSE)
+groen_bp <- read.delim("topGO_BP_groen_results.tsv", sep = "\t", stringsAsFactors = FALSE)
+
+hap2_goids_bp <- hap2_bp$GO.ID
+groen_goids_bp <- groen_bp$GO.ID
+
+# 差异项
+hap2_only_bp <- setdiff(hap2_goids_bp, groen_goids_bp)
+groen_only_bp <- setdiff(groen_goids_bp, hap2_goids_bp)
+common_goids_bp <- intersect(hap2_goids_bp, groen_goids_bp)
+
+# 输出数量
+cat("hap2 独有 GO terms (BP):", length(hap2_only_bp), "\n")
+cat("groen 独有 GO terms (BP):", length(groen_only_bp), "\n")
+cat("共有 GO terms (BP):", length(common_goids_bp), "\n")
+
+hap2_only_terms_bp <- hap2_bp[hap2_bp$GO.ID %in% hap2_only_bp, c("GO.ID", "Term")]
+groen_only_terms_bp <- groen_bp[groen_bp$GO.ID %in% groen_only_bp, c("GO.ID", "Term")]
+
+print(hap2_only_terms_bp)
+print(groen_only_terms_bp)
+
+
+library(VennDiagram)
+
+venn.diagram(
+  x = list(hap2 = hap2_goids_bp, groen = groen_goids_bp),
+  category.names = c("hap2", "groen"),
+  filename = "Venn_GO_BP_hap2_vs_groen_colored.png",
+  fill = c("skyblue", "salmon"),
+  output = TRUE
+)
+
+
