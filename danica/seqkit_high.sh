fd429944ea07fbaed39f2ae8f23947792af05c69
Junfang H <alyjh38@nottingham.ac.uk>
2025-07-28 13:50:59 +0100

    Update seqkit_high.sh

--- danica/seqkit_high.sh
+++ danica/seqkit_high.sh
@@ -1,2 +1,3 @@
-seqkit grep -f high_identity_groen_ids.txt hap2_large_contigs.faa -o hap2_highid.faa
-[INFO] 9804 patterns loaded from file
+$ seqkit grep -f high_identity_hap2_ids.txt \
+>             hap2_large_contigs.faa \
+>             -o hap2_highid.faa
