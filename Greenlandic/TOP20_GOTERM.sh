244d1c6bfb3e54b0b63a35720e75626fd5373b6c
Junfang H <alyjh38@nottingham.ac.uk>
2025-07-29 10:34:40 +0100

    Create TOP20_GOTERM.sh

---
+++ Greenlandic/TOP20_GOTERM.sh
@@ -0,0 +1,53 @@
+library(dplyr)
+top_go <- gene2go_enricher %>%
+  count(GO_ID, sort = TRUE) %>%
+  top_n(20, n)  # 取前20条
+
+desc_vec <- c(
+  "nucleic acid binding",
+  "DNA integration",
+  "cellular process",
+  "RNA-dependent DNA biosynthetic process",
+  "RNA-directed DNA polymerase activity",
+  "membrane",
+  "endonuclease activity",
+  "aspartic-type endopeptidase activity",
+  "zinc ion binding",
+  "nucleus",
+  "calcium ion transmembrane transport",
+  "oxidoreductase activity",
+  "response to stimulus",
+  "mitochondrion",
+  "ATP binding",
+  "integral component of membrane",
+  "protein homodimerization activity",
+  "DNA replication initiation",
+  "external encapsulating structure",
+  "external encapsulating structure organization",
+  "cell projection membrane",
+  "cell projection part",
+  "cell projection",
+  "ATP-dependent DNA helicase activity",
+  "DNA-directed DNA polymerase activity",
+  "organelle membrane",
+  "DNA packaging",
+  "ribonuclease H activity",
+  "RNA-DNA hybrid ribonuclease activity",
+  "DNA recombination",
+  "DNA repair",
+  "DNA binding",
+  "regulation of transcription from RNA pol II promoter",
+  "proteolysis",
+  "spindle microtubule",
+  "response to mechanical stimulus",
+  "chromatin organization"
+)
+
+top_go$desc <- desc_vec
+
+library(ggplot2)
+ggplot(top_go, aes(x = reorder(desc, n), y = n)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  coord_flip() +
+  labs(x = "GO Function", y = "Gene Count", title = "Top 20 GO Functions of Expanded Genes") +
+  theme_minimal()
