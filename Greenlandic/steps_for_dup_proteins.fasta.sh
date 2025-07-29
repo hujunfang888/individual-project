488d4040c4ccb719d2e11c80cace8d9f718bfda1
Junfang H <alyjh38@nottingham.ac.uk>
2025-07-29 10:00:55 +0100

    Update steps_for_dup_proteins.fasta.sh

--- Greenlandic/steps_for_dup_proteins.fasta.sh
+++ Greenlandic/steps_for_dup_proteins.fasta.sh
@@ -1,15 +1,24 @@
-awk -F'\t' 'NR>1 {
-  # 6 列是 Genes.1，7 列是 Genes.2
-  n=split($6,a,","); for(i=1;i<=n;i++) print a[i]
-  n=split($7,b,","); for(i=1;i<=n;i++) print b[i]
-}' Duplications.tsv \
-| sed -e 's/^ *//g' -e 's/ *$//g' \
-      -e 's/^groen_//' -e 's/^hap2_//' \
-| sort -u > dup_ids.txt
+# 已有：groen_dup_ids.txt，包含 4780 条 NCJMDBGG_… ID
+wc -l groen_dup_ids.txt   # 应显示 4780
+head -n 5 groen_dup_ids.txt
 
-cat \
->   hap2_large_contigs.faa \
->   prokka_output/groenlandica_large_contigs.faa \
-> > proteome.fasta
+cd prokka_output
 
-seqkit grep -f dup_ids.txt proteome.fasta -o dup_proteins.fasta
+awk '
+  NR==FNR { ids[$1]; next }      # 先读 groen_dup_ids.txt，将每个 ID 存到数组 ids
+  /^>/ {
+    # 提取 header 行的 ID：>NCJMDBGG_00001   hypothetical...
+    # 用空白分割，取第一个字段，去掉 leading ">"
+    split($0, fields, /[ \t]/);
+    id = substr(fields[1], 2);
+    keep = (id in ids);
+  }
+  keep                            # 如果 keep 为真，则打印该行及后续序列行
+' ../groen_dup_ids.txt groenlandica_large_contigs.faa \
+  > ../dup_proteins.fasta
+
+cd ..
+ls -lh dup_proteins.fasta          # 确认大小非 0
+grep "^>" dup_proteins.fasta | head -n 10
+grep "^>" dup_proteins.fasta | wc -l  # 应约等于 4780
+
