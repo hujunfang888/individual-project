83f1e06a9fab508a09f36ba3396375e307cc9325
Junfang H <alyjh38@nottingham.ac.uk>
2025-08-09 16:09:52 +0100

    Update steps_for_dup_proteins.fasta.sh

--- Greenlandic/steps_for_dup_proteins.fasta.sh
+++ Greenlandic/steps_for_dup_proteins.fasta.sh
@@ -1,24 +1,24 @@
-# 已有：groen_dup_ids.txt，包含 4780 条 NCJMDBGG_… ID
-wc -l groen_dup_ids.txt   # 应显示 4780
+#  Already have: groen_dup_ids.txt, containing 4780 NCJMDBGG_… ID
+wc -l groen_dup_ids.txt   # dispaly 4780
 head -n 5 groen_dup_ids.txt
 
 cd prokka_output
 
 awk '
-  NR==FNR { ids[$1]; next }      # 先读 groen_dup_ids.txt，将每个 ID 存到数组 ids
+  NR==FNR { ids[$1]; next }      # read groen_dup_ids.txt first, save each ID to the array ids
   /^>/ {
-    # 提取 header 行的 ID：>NCJMDBGG_00001   hypothetical...
-    # 用空白分割，取第一个字段，去掉 leading ">"
+    # extract the header row ID：>NCJMDBGG_00001   hypothetical...
+    # use blank to split, take the first field, and remove itleading ">"
     split($0, fields, /[ \t]/);
     id = substr(fields[1], 2);
     keep = (id in ids);
   }
-  keep                            # 如果 keep 为真，则打印该行及后续序列行
+  keep                            # if keep is true, print the line and subsequent sequence lines
 ' ../groen_dup_ids.txt groenlandica_large_contigs.faa \
   > ../dup_proteins.fasta
 
 cd ..
-ls -lh dup_proteins.fasta          # 确认大小非 0
+ls -lh dup_proteins.fasta          # confirm size non-0
 grep "^>" dup_proteins.fasta | head -n 10
-grep "^>" dup_proteins.fasta | wc -l  # 应约等于 4780
+grep "^>" dup_proteins.fasta | wc -l  # equals 4780
 
