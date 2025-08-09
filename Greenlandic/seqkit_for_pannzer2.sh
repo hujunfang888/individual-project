1701ae02470c06d76eeeb84ec6eefa3d60227de3
Junfang H <alyjh38@nottingham.ac.uk>
2025-08-09 16:07:30 +0100

    Update and rename seqkit.sh to seqkit_for_pannzer2.sh

--- Greenlandic/seqkit_for_pannzer2.sh
+++ Greenlandic/seqkit_for_pannzer2.sh
@@ -1,25 +1,23 @@
 
-# 读取orthogroup信息和低相似度OG列表
+# read orthogroup information and low similarity OG list
 orthos <- read.table("Orthogroups.tsv", header = TRUE, sep = "\t", stringsAsFactors = FALSE, quote = "")
 lowid <- read.table("low_id_otho_gro_hap2.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 
-# 保证列名正确
 colnames(orthos)[1:3] <- c("Orthogroup", "groen", "hap2")
 colnames(lowid)[1] <- "Orthogroup"
 
-# 只保留低相似度OG
+# only save low similarity
 target_og <- orthos[orthos$Orthogroup %in% lowid$Orthogroup, ]
 
-# 分别获取groen和hap2所有蛋白ID（去掉逗号和空格，展开为一列）
+# obtain all protein IDs of groen and hap2 respectively
 library(tidyr)
 groen_ids <- unlist(strsplit(paste(target_og$groen, collapse=","), ",\\s*"))
 hap2_ids  <- unlist(strsplit(paste(target_og$hap2, collapse=","), ",\\s*"))
 
-# 去重（可选）
+
 groen_ids <- unique(groen_ids[groen_ids != ""])
 hap2_ids  <- unique(hap2_ids[hap2_ids != ""])
 
-# 写入文件
 writeLines(groen_ids, "groen_lowid.txt")
 writeLines(hap2_ids,  "hap2_lowid.txt")
 
